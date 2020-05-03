5cc4b62d0bf02316487c56a5f9c1693c2d363926
Asgar <asgarkhan21@gmail.com>
2020-05-03 12:44:40 +0800

    Modification of static Message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ console.log('KnowYourDevice')
 console.log(process.argv[2])
 const device = {Device: 'Samsung', Model:'Note9'}
 setTimeout(() => {
-    console.log('2 Second Timer')
+    console.log('2 Second Timer called')
 }, 20000)
 console.log(JSON.stringify(device))
 const parse =JSON.parse(JSON.stringify(device))
