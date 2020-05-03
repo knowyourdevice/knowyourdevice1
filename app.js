3457402de84cdbb3da48e0f7f4094eba4c55a06d
Asgar <asgarkhan21@gmail.com>
2020-05-03 12:41:25 +0800

    Intial Commit

---
+++ app.js
@@ -0,0 +1,9 @@
+console.log('KnowYourDevice')
+console.log(process.argv[2])
+const device = {Device: 'Samsung', Model:'Note9'}
+setTimeout(() => {
+    console.log('2 Second Timer')
+}, 20000)
+console.log(JSON.stringify(device))
+const parse =JSON.parse(JSON.stringify(device))
+console.log(parse.Model)
